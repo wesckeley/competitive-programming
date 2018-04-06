4d8a393f1b5e65cdc3a4cf42118d1006db9e2662
Wesckeley de freitas Martins <wesckeley.martins@medpej.local>
2018-04-06 15:34:18 -0300

    hackerhank-security

--- hackerhank/security/security-inverse-of-a-function.py
+++ hackerhank/security/security-inverse-of-a-function.py
@@ -1,2 +1,10 @@
 n = int(input())
 function = [int(x) for x in input().split(' ')]
+
+inverse = [0 for x in range(0,n)]
+
+for i in range(0,n):
+    inverse[function[i]-1] = i+1
+
+for i in range(0,n):
+    print(inverse[i])
--- hackerhank/security/security-message-space-and-ciphertext-space.py
+++ hackerhank/security/security-message-space-and-ciphertext-space.py
@@ -4,4 +4,4 @@ answer = ""
 for i in range(0,len(line)):
     answer += chr(((int(line[i]) + 1) % 10) + ord('0'))
 
-print(answer)
+print(answer)   
