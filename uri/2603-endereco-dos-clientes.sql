d4d9c8b81b91a77bea98bf3afb461a57e36c4895
wesckeley <wesckeley@gmail.com>
2018-04-14 20:20:58 -0300

    sql

--- uri/2603-endereco-dos-clientes.sql
+++ uri/2603-endereco-dos-clientes.sql
@@ -1,7 +1,7 @@
 --- URI Online Judge SQL
 --- Copyright URI Online Judge
 --- www.urionlinejudge.com.br
---- Problem 2602
+--- Problem 2603
 
 CREATE TABLE customers (
   id NUMERIC PRIMARY KEY,
@@ -13,16 +13,18 @@ CREATE TABLE customers (
 );
 
 INSERT INTO customers (id, name, street, city, state, credit_limit)
-VALUES 
+VALUES
   (1,	'Pedro Augusto da Rocha',	'Rua Pedro Carlos Hoffman',	'Porto Alegre',	'RS',	700.00),
-  (2,	'Antonio Carlos Mamel',	'Av. Pinheiros', 'Belo Horizonte',	'MG',	3500.50),
-  (3,	'Luiza Augusta Mhor',	'Rua Salto Grande',	'Niteroi',	'RJ',	4000.00),	
+  (2,	'Antonio Carlos Mamel',	'Av. Pinheiros',	'Belo Horizonte',	'MG',	3500.50),	
+  (3,	'Luiza Augusta Mhor,',	'Rua Salto Grande',	'Niteroi',	'RJ',	4000.00),
   (4,	'Jane Ester',	'Av 7 de setembro',	'Erechim',	'RS',	800.00),
-  (5, 'Marcos Antônio dos Santos',	'Av Farrapos',	'Porto Alegre',	'RS',	4250.25);
-
+  (5,	'Marcos Antônio dos Santos',	'Av Farrapos',	'Porto Alegre',	'RS',	4250.25);
+  
 -- Begin of the submitted code
 
-SELECT name FROM customers WHERE state = 'RS';
+SELECT name, street 
+FROM customers 
+WHERE city = 'Porto Alegre';
 
 -- End of the submitted code
 
